8796915775751b390816deca8a1b988f35adfc90
Sawyer <sawyer.welter@gmail.com>
2023-04-12 18:57:10 -0500

    Resource post functionality implemented

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,13 +1,25 @@
 // build your `Resource` model here
 const db = require('../../data/dbConfig');
 
-/*  [POST] /api/resources
 
-Example of response body: {"resource_id":1,"resource_name":"foo","resource_description":null}
- [GET] /api/resources
+async function get(id) {
+    return await db('resources').where('resource_id', id).first();
+}
 
-Example of response body: [{"resource_id":1,"resource_name":"foo","resource_description":null}] */
+// returns all resources as an array 
+async function getAll() {
+    return await db('resources');
+}
+
+// creates new resource from given argument
+// and returns the new resource from the db
+async function create(resource){
+    const [resource_id] = await db('resources').insert(resource);
+    const newResource = get(resource_id);
+    return newResource;
+}
 
 module.exports = {
-    
+    getAll,
+    create
 }
--- api/resource/router.js
+++ api/resource/router.js
@@ -3,11 +3,40 @@ const router = require('express').Router();
 const Resource = require('./model');
 
 
-/*  [POST] /api/resources
+/* [GET] /api/resources
+    Example of response body: 
+    [
+        {
+            "resource_id":1,
+            "resource_name":"foo",
+            "resource_description":null
+        }
+    ]
+*/
+router.get('/', (req, res, next) => {
+    Resource.getAll()
+        .then(resources => {
+            res.status(200).json(resources);
+        })
+        .catch();
+})
 
-Example of response body: {"resource_id":1,"resource_name":"foo","resource_description":null}
- [GET] /api/resources
+/*  [POST] /api/resources 
+        resource_name (REQUIRED)
+        resource_description (optional)
+*/
+router.post('/', (req, res, next) => {
+    const { resource_name } = req.body;
+    if(!resource_name) {
+        res.status(400).json({message: "resource_name required"})
+    } else {
+        Resource.create(req.body)
+            .then(resource => {
+                res.status(202).json(resource);
+            })
+            .catch(next);
+    }
+})
 
-Example of response body: [{"resource_id":1,"resource_name":"foo","resource_description":null}] */
 
 module.exports = router;
