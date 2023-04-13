0247331e32bde48ee554ac9362bf436fd19d91ad
Sawyer <sawyer.welter@gmail.com>
2023-04-12 19:23:16 -0500

    router and model get/post for tasks created and implemented

--- api/task/model.js
+++ api/task/model.js
@@ -1,16 +1,64 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig');
 
-/* [POST] /api/tasks
 
-Even though task_completed is stored as an integer, the API uses booleans when interacting with the client
-Example of response body: {"task_id":1,"task_description":"baz","task_notes":null,"task_completed":false,"project_id:1}
- [GET] /api/tasks
+// returns data for a task at a given id
+async function get(task_id) {
+    return await db('tasks').where('task_id', task_id).first();
+}
 
-Even though task_completed is stored as an integer, the API uses booleans when interacting with the client
-Each task must include project_name and project_description
-Example of response body: [{"task_id":1,"task_description":"baz","task_notes":null,"task_completed":false,"project_name:"bar","project_description":null}]*/
+
+/* Each task must include project_name and project_description; task_completed should be a boolean, not an integer.
+
+    Example of response body: 
+        [
+            {
+                "task_id":1,
+                "task_description":"baz",
+                "task_notes":null,
+                "task_completed":false,
+                "project_name:"bar","project_description":null
+            }
+        ]
+*/
+async function getAll() {
+    const tasks = 
+        await db('tasks as t')
+                .select(
+                    't.*', 
+                    'p.project_name', 
+                    'p.project_description')
+                .leftJoin('projects as p', 't.project_id', '=', 'p.project_id');
+    
+
+    for(const task of tasks) {
+        task.task_completed = !!task.task_completed;
+    }
+
+    return tasks;
+}
+
+
+/* 
+Example of response body: 
+    {
+        "task_id":1,
+        "task_description":"baz",
+        "task_notes":null,
+        "task_completed":false,
+        "project_id":1
+    }
+*/ 
+async function create(task) {
+    const [task_id] = await db('tasks').insert(task);
+    const newTask = await get(task_id);
+    newTask.task_completed = !!newTask.task_completed;
+
+    return newTask;
+}
+
 
 module.exports = {
-    
+    getAll, 
+    create
 }
--- api/task/router.js
+++ api/task/router.js
@@ -12,5 +12,21 @@ Even though task_completed is stored as an integer, the API uses booleans when i
 Each task must include project_name and project_description
 Example of response body: [{"task_id":1,"task_description":"baz","task_notes":null,"task_completed":false,"project_name:"bar","project_description":null}]*/
 
+    router.get('/', (req, res, next) => {
+        Task.getAll()
+            .then(tasks => {
+                res.status(200).json(tasks);
+            })
+            .catch(next);
+    });
+
+
+    router.post('/', (req, res, next) => {
+        Task.create(req.body)
+            .then(task => {
+                res.status(202).json(task);
+            })
+            .catch(next);
+    })
 
 module.exports = router;
